8451f4695ae35a4cae57304dd8e1a3d07fc74de0
Dronifion <dronifion@gmail.com>
2022-02-13 14:46:22 +0800

    Disable the character's collider function when it hits the ceiling

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 5f;    
+    GameObject currentFloor; //To store current floor object
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +15,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //When player press right arrow key
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            //Move player's character to the right
             transform.Translate(moveSpeed*Time.deltaTime,0,0);
         }
+        //When player press left arrow key
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            //Move player's character to the left
             transform.Translate(-moveSpeed*Time.deltaTime,0,0);    
         }
         
@@ -27,19 +32,33 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) 
     {
+        //When player's character hits normal floor object with collision
         if (other.gameObject.tag == "Normal")
         {
             Debug.Log("Hit normal floor");
+            //Set the currentFloor object to the normal floor object
+            currentFloor = other.gameObject;
         }   
+        //When player's character hits nails floor object with collision
         else if (other.gameObject.tag == "Nails")
         {
             Debug.Log("Hit nails floor");
-        } 
+            //Set the currentFloor object to the nails floor object
+            currentFloor = other.gameObject;
+        }         
+        //When player's character hits ceiling object with collision
+        else if (other.gameObject.tag == "Ceiling")
+        {
+            Debug.Log("Hit ceiling");
+            //Disable the character's collider function
+            currentFloor.GetComponent<BoxCollider2D>().enabled = false;
+        }         
 
     }
 
     void OnTriggerEnter2D(Collider2D other) 
     {
+        //When player's character falls down and hits by triggering the death line
         if (other.gameObject.tag == "DeathLine")
         {
             Debug.Log("Die");
