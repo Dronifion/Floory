f8173bef27ec469a5a5b05a9211a06064819799b
Dronifion <dronifion@gmail.com>
2022-02-14 17:37:14 +0800

    Added game score and are able to display HP and score on scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;    
     [SerializeField] int Hp; //Character's HP
+    [SerializeField] int MaxHp; //Character's maximum HP
+    [SerializeField] Text LifeText; //Game Life
+    [SerializeField] Text ScoreText; //Game Score
     GameObject currentFloor; //To store current floor object
+    int Score = 0; //To record game score
+    float scoreTime = 0f; //Use to calculate score with time passed in game
 
     // Start is called before the first frame update
     void Start()
     {
+        //Initial character's HP and max HP values to 10
         Hp = 10;
+        MaxHp = 10;
+
+        //Initial game hp and score and then display on scene
+        LifeText.text = "Life : " + Hp.ToString() + " / " + MaxHp.ToString();
+        ScoreText.text = "Floor 0";
     }
 
     // Update is called once per frame
@@ -30,6 +42,8 @@ public class Player : MonoBehaviour
             transform.Translate(-moveSpeed*Time.deltaTime,0,0);    
         }
         
+        //Update game score
+        UpdateScore();
     }
 
     void OnCollisionEnter2D(Collision2D other) 
@@ -96,6 +110,22 @@ public class Player : MonoBehaviour
         else if (Hp <= 0 )
         {
             Hp = 0;
+        }
+
+        //Display current HP on scene
+        LifeText.text = "Life : " + Hp.ToString() + " / " + MaxHp.ToString();
+    }
+
+    void UpdateScore()
+    {
+        scoreTime += Time.deltaTime;
+
+        //Add score every 2 second time passes on game.
+        if (scoreTime > 2f)
+        {
+            Score++;
+            scoreTime = 0f;
+            ScoreText.text = "Floor " + Score.ToString();
         }
     }
 }
